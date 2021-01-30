35926402ba969adede24ed45714d75edb70934c0
Yoggi-Baba <yrathod33@gmail.com>
2021-01-30 10:26:05 +0530

    Updates

---
+++ precache-manifest.408649186b8d027bda43b14200583dd4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8e915dab6f5839e48aa15079a15543",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "f53f99bc9d675e7581e6",
+    "url": "/developerFolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "b72c60fbcdcc1dece2fe",
+    "url": "/developerFolio/static/css/4.21259c32.chunk.css"
+  },
+  {
+    "revision": "6c086fde2209b0ddce24",
+    "url": "/developerFolio/static/css/main.fed0e925.chunk.css"
+  },
+  {
+    "revision": "b02cd79dbf565d4bd2f8",
+    "url": "/developerFolio/static/js/2.01261c30.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/developerFolio/static/js/2.01261c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53f99bc9d675e7581e6",
+    "url": "/developerFolio/static/js/3.db5f2624.chunk.js"
+  },
+  {
+    "revision": "b72c60fbcdcc1dece2fe",
+    "url": "/developerFolio/static/js/4.c32368b7.chunk.js"
+  },
+  {
+    "revision": "6c086fde2209b0ddce24",
+    "url": "/developerFolio/static/js/main.1c34eb10.chunk.js"
+  },
+  {
+    "revision": "13dcdbe9a6039fc4dc87",
+    "url": "/developerFolio/static/js/runtime-main.1260aab4.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1089433819161d8e70df907632b36a4a",
+    "url": "/developerFolio/static/media/Pict_logo.10894338.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/developerFolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "d3a26928d532e6ed8d8c872ba6075540",
+    "url": "/developerFolio/static/media/ubisoft.d3a26928.png"
+  }
+]);
